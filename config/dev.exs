5f2e288c093dfe5113aaed26cb572487698b8a87
Ozgun Atacan <ozgun.atacan@gmail.com>
2019-12-21 15:16:16 +0300

    db config dev

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :quick_poll, QuickPoll.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "pass",
   database: "quick_poll_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
