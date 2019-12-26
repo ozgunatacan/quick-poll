9075eb0cda644e016322212dd11c2fb57bbf787b
Ozgun Atacan <ozgun.atacan@gmail.com>
2019-12-26 15:07:02 +0300

    added sobelow framework

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule QuickPoll.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:ex_machina, "~> 2.3", only: :test}
+      {:ex_machina, "~> 2.3", only: :test},
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 
